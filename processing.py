4b895c6a4b5efe80ce3dca4e473fddc3f2a0a7cb
ABHISHEK SUGA <abhisheksuga@ABHISHEKs-Air.fritz.box>
2024-04-18 19:13:04 +0100

    added few more functions which aids in processing the data, added few more columns to the dataframe which helps us do  analysis better, also adding the processed dataframe to s3 bucket

--- processing.py
+++ processing.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 import time
 import pandas as pd
 from textblob import TextBlob
-
+from utility import day_mapping
 
 def count_of_words(content):
     return len(content.split())
@@ -44,6 +44,18 @@ df = df.drop(columns = ['id','latitude','longitude','country'])
 df['num_words'] = df['content'].apply(count_of_words)
 df['sentiment_polarity'] = df['content'].apply(extract_sentiment)
 df['sentiment_category'] = df['sentiment_polarity'].apply(sentiment_category)
+df['date_time'] = pd.to_datetime(df['date_time'], format='%d/%m/%Y %H:%M')   #converting to standart date time format so that we can use .dt accessor of pandas
+df['activity_hour']= df['date_time'].dt.hour
+df['activity_day']= df['date_time'].dt.dayofweek
+df['activity_month']= df['date_time'].dt.month
+df['activity_year'] = df ['date_time'].dt.year
+df['engagement'] = df['number_of_likes'] + df ['number_of_shares']
+df['activity_day'] = df['activity_day'].map(day_mapping())
 
+# print(df['activity_day'].dtype)
+# print(df['activity_day'].unique())
+# print(df.head())
 
-print(df.head())
+df.to_csv('s3://abhi-data/data/tweets_processed.csv')
+
+print("data successfully uploaded to the s3 bucket")
