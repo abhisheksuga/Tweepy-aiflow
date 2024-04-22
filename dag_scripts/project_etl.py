63c557974136f9ae2a389e1ff093575090669626
ABHISHEK SUGA <abhisheksuga@ABHISHEKs-Air.fritz.box>
2024-04-22 17:19:24 +0100

    changes made in the insertion function for retaining the original datatypes as in the csv file

--- dag_scripts/project_etl.py
+++ dag_scripts/project_etl.py
@@ -9,7 +9,7 @@ import time
 
 # Function to download desired dataset from kaggle using kaggle api 
 def download_kaggle_dataset(dataset_name ,download_path,unzip=False) :
-    
+
     try:
         api = KaggleApi()
         api.authenticate()
@@ -18,18 +18,18 @@ def download_kaggle_dataset(dataset_name ,download_path,unzip=False) :
     except Exception as e:
         print (f"error while downloading: {e}")
 
-def mongo_insert(mongo_uri,db_name,collection_name)
+def mongo_insert(mongo_uri,db_name,collection_name):
     client = MongoClient(mongo_uri)
     db = client[db_name]
     collection = db[collection_name]
     start_time = time.time()
 
-    with open('./data/tweets.csv','r') as csvfile :
-        reader = csv.DictReader(csvfile)
-        data = list(reader)
-        collection.insert_many(data)
+    df = pd.read_csv('./data/tweets.csv')
+    data = df.to_dict(orient='records')
+    collection.insert_many(data)
 
     end_time = time.time()
-    print (f"The taken to insert all the records using inssert_one query is ,{end_time - start_time}")
+    print (f"The taken to insert all the records using insert_one query is ,{end_time - start_time}")
     client.close()
-                           
+
+
